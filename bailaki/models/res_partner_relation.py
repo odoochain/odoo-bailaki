9125cdcf356cd2c9312b7e4e4d6a09f2546e3181
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-02-15 22:20:25 -0300

    [FIX] date unmatches

--- bailaki/models/res_partner_relation.py
+++ bailaki/models/res_partner_relation.py
@@ -9,12 +9,6 @@ class ResPartnerRelation(models.Model):
 
     _inherit = 'res.partner.relation'
 
-    date_start = fields.Date(
-        'Starting date',
-        required=True,
-        default=fields.Date.today
-    )
-
     @api.model
     def _cron_unmatch_relation_clean(self):
         """
@@ -23,6 +17,6 @@ class ResPartnerRelation(models.Model):
         unmatch_id = self.env.ref("bailaki.relation_type_unmatch")
 
         for relation in self.search([('type_id', '=', unmatch_id.id)]):
-            if (relation.date_start):
-                if (relation.date_start + timedelta(days=30) < fields.date.today()):
+            if (relation.create_date):
+                if (relation.create_date + timedelta(days=30) < fields.datetime.now()):
                     relation.unlink()
