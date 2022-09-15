be07acad2d6af71651994a739caa23bb564fe28f
Tiago Magrini Rigo <tiagomrigo@gmail.com>
2022-09-15 15:18:13 -0300

    website modifications

--- bailaki/__manifest__.py
+++ bailaki/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Bailaki',
     'description': """
         Bailaki Backend""",
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'license': 'AGPL-3',
     'author': 'Marcel Savegnago & Marcos Méndez',
     'website': 'https://popsolutions.co',
@@ -16,16 +16,18 @@
         'partner_contact_nationality',
         'partner_contact_gender',
         'event',
+        'hermes-message',
     ],
     'data': [
-        'security/ir.model.access.csv',
         'security/res_partner.xml',
         'security/res_partner_music_skill.xml',
         'security/res_partner_music_genre.xml',
+        'security/ir.model.access.csv',
         'views/res_partner_music_skill.xml',
         'views/res_partner_music_genre.xml',
         'views/res_partner.xml',
         'views/templates.xml',
+        'views/website_templates.xml',
         'data/res_partner_relation.xml',
     ],
     'demo': [
